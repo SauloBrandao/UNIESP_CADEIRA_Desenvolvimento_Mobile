f8e041a9a9aef9380ff6e6749c577404be54d8c9
SauloBrandao <2023211510080@iesp.edu.br>
2025-10-09 02:50:45 -0300

    Feat: Terminando Cartão Perfil

--- aula02/App.js
+++ aula02/App.js
@@ -2,11 +2,12 @@ import { View} from 'react-native'
 import React from 'react'
 import LayoutResponsive from './src/components/LayoutResponsive'
 import CartaoApresentacao from './src/components/CartaoApresentacao'
+import CartaoPerfil from './src/components/CartaoPerfil'
 
 const App = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <CartaoApresentacao />
+      <CartaoPerfil />
     </View>
   )
 }
