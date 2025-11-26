90fc82b5c7b864b572ed63044a4494d38d321f4c
SauloBrandao <2023211510080@iesp.edu.br>
2025-11-26 09:25:21 -0300

    feat: Mockando BD

--- aula03/App.js
+++ aula03/App.js
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import LoginScreen from './src/screens/LoginScreen'
+import HomeScreen from './src/screens/HomeScreen'
 
 const App = () => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <LoginScreen></LoginScreen>
+    <View style={{flex: 1}}>
+      <HomeScreen></HomeScreen>
     </View>
   )
 }
--- aula03/src/screens/HomeScreen.js
+++ aula03/src/screens/HomeScreen.js
@@ -2,9 +2,19 @@ import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const HomeScreen = () => {
+
+    const items = [
+        { id: '1', name: 'Curso de React Native', description: 'Aprenda a criar apps para Android e iOS' },
+        { id: '2', name: 'Curso de Java Spring Boot', description: 'Construa APIs robusta com Java e Spring'},
+        { id: '3', name: 'Curso de AWS ', description: 'Domine os serviços da AWS'},
+        { id: '4', name: '', description: 'Construa APIs robusta com Java e Spring'}
+    ]
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+        <Text style={styles.title}>📚 Cursos Disponíveis </Text>
+
+        <View></View>
     </View>
   )
 }
