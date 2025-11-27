9943e77f010a9336de5e0aab18f60cad92c208b9
SauloBrandao <2023211510080@iesp.edu.br>
2025-11-26 23:44:37 -0300

    feat: criando navegação

--- aula03/App.js
+++ aula03/App.js
@@ -2,11 +2,20 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import LoginScreen from './src/screens/LoginScreen'
 import HomeScreen from './src/screens/HomeScreen'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { StackScreen } from 'react-native-screens'
+
+const Stack = createNativeStackNavigator()
 
 const App = () => {
   return (
     <View style={{flex: 1}}>
-      <HomeScreen></HomeScreen>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Login' screenOptions={{ headerTitleAlign: 'center'}}>
+          <Stack.Screen/>
+        </Stack.Navigator>
+      </NavigationContainer>
     </View>
   )
 }
